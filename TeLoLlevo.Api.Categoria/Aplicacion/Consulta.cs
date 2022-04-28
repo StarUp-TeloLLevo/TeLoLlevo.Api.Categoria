f9f8977963585e57b3516ac2dcca376de0ff246e
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-04-27 22:35:58 -0500

    Se agrego la clase consulta, ademas de agregar la clase de mapeo, y la clase categoriasDto

---
+++ TeLoLlevo.Api.Categoria/Aplicacion/Consulta.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TeLoLlevo.Api.Categoria.Dto;
+using TeLoLlevo.Api.Categoria.Persistencia;
+
+namespace TeLoLlevo.Api.Categoria.Aplicacion
+{
+    public class Consulta
+    {
+        public class Ejecuta : IRequest<List<CategoriasDto>> { 
+
+        }
+        public class Manejador : IRequestHandler<Ejecuta, List<CategoriasDto>>
+        {
+            private readonly CategoriasContexto _contexto;
+            private readonly IMapper _mapper;
+            public Manejador(CategoriasContexto contexto, IMapper mapper)
+            {
+                _contexto = contexto;
+                _mapper = mapper;
+            }
+
+            public Task<List<CategoriasDto>> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                
+            }
+        }
+    }
+}
---
+++ TeLoLlevo.Api.Categoria/Mapper/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeLoLlevo.Api.Categoria.Dto;
+using TeLoLlevo.Api.Categoria.Modelo;
+
+namespace TeLoLlevo.Api.Categoria.Mapper
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Categorias, CategoriasDto>().ReverseMap();
+        }
+    }
+}
