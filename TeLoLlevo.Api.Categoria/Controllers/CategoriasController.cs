57633b6aa5712a7be09f86a300b0083e5ccc66d1
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-04-27 23:14:03 -0500

    se genero el metodo de consultar en el controlador de categorias

--- TeLoLlevo.Api.Categoria/Controllers/CategoriasController.cs
+++ TeLoLlevo.Api.Categoria/Controllers/CategoriasController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TeLoLlevo.Api.Categoria.Aplicacion;
+using TeLoLlevo.Api.Categoria.Dto;
 
 namespace TeLoLlevo.Api.Categoria.Controllers
 {
@@ -23,6 +24,16 @@ namespace TeLoLlevo.Api.Categoria.Controllers
         public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data)
         {
             return await _mediator.Send(data);
+        }
+
+        /// <summary>
+        /// Este metodo se encarga de
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<List<CategoriasDto>>> Consultar()
+        {
+            return await _mediator.Send( new Consulta.Ejecuta() );
         }
     }
 }
